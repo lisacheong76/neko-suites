fe2f550476b7bded4bb94a279c370579c16aa329
Lisa <lisacheong.danial@gmail.com>
2022-05-10 13:49:18 +0800

    gender option picker

--- src/views/screens/EditUserProfile.js
+++ src/views/screens/EditUserProfile.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigation } from '@react-navigation/core';
-import { Share, View, SafeAreaView, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/core";
+import { Share, View, SafeAreaView, StyleSheet, Picker } from "react-native";
 import {
   Avatar,
   Title,
@@ -8,18 +8,18 @@ import {
   Text,
   TextInput,
   TouchableRipple,
-} from 'react-native-paper';
-import COLORS from '../../consts/colors';
-import Icon2 from 'react-native-vector-icons/MaterialIcons';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import Icon3 from 'react-native-vector-icons/FontAwesome5';
+} from "react-native-paper";
+import COLORS from "../../consts/colors";
+import Icon2 from "react-native-vector-icons/MaterialIcons";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import Icon3 from "react-native-vector-icons/FontAwesome5";
 import {
   auth,
   firestore,
   getStorage,
   ref,
   getDownloadURL,
-} from '../../../firebase';
+} from "../../../firebase";
 
 // import Share from 'react-native-share';
 
@@ -33,14 +33,15 @@ const EditUserProfile = () => {
   const photo = auth.currentUser.photoURL;
   const phoneNumber = auth.currentUser.phoneNumber;
 
-  const [userData, setUserData] = useState('');
-  const [url, setUrl] = useState('');
+  const [userData, setUserData] = useState("");
+  const [url, setUrl] = useState("");
+  const [selectedValue, setSelectedValue] = useState("");
 
   const getUser = async () => {
-    const userRef = firestore.collection('users').doc(auth.currentUser.uid);
+    const userRef = firestore.collection("users").doc(auth.currentUser.uid);
     const doc = await userRef.get();
     if (!doc.exists) {
-      console.log('No such document!');
+      console.log("No such document!");
     } else {
       setUserData(doc.data());
     }
@@ -82,7 +83,7 @@ const EditUserProfile = () => {
         <Icon2
           name="arrow-back-ios"
           size={28}
-          color={'#665444'}
+          color={"#665444"}
           onPress={navigation.goBack}
         />
       </View>
@@ -90,22 +91,22 @@ const EditUserProfile = () => {
         <View style={styles.userInfoSection}>
           <View
             style={{
-              flexDirection: 'row',
+              flexDirection: "row",
               marginTop: 15,
-              alignItems: 'center',
-              justifyContent: 'center',
+              alignItems: "center",
+              justifyContent: "center",
             }}
           >
             <Avatar.Image source={{ uri: url }} size={90} />
           </View>
-          <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <View style={{ justifyContent: "center", alignItems: "center" }}>
             <Text
               style={{
                 marginTop: 10,
                 marginBottom: 5,
-                color: '#665444',
+                color: "#665444",
                 fontSize: 15,
-                fontWeight: 'bold',
+                fontWeight: "bold",
               }}
             >
               Profile Picture
@@ -137,7 +138,7 @@ const EditUserProfile = () => {
               placeholderTextColor="#666666"
               placeholderTextSize="20"
               autoCorrect={false}
-              value={userData.name || ''}
+              value={userData.name || ""}
             ></TextInput>
           </View>
         </View>
@@ -150,21 +151,24 @@ const EditUserProfile = () => {
               placeholderTextColor="#666666"
               placeholderTextSize="20"
               autoCorrect={false}
-              value={phoneNumber || ''}
+              value={phoneNumber || ""}
             ></TextInput>
           </View>
         </View>
         <View style={styles.row}>
           <View style={styles.textBox}>
             <Icon name="human-male-female" color="#665444" size={20} />
-            <TextInput
-              style={styles.editTextBox}
-              placeholder="Gender"
-              placeholderTextColor="#666666"
-              placeholderTextSize="20"
-              autoCorrect={false}
-              value={userData.gender || ''}
-            ></TextInput>
+            <Picker
+              selectedValue={selectedValue}
+              style={{ height: 50, width: 310 }}
+              onValueChange={(itemValue, itemIndex) =>
+                setSelectedValue(itemValue)
+              }
+            >
+              <Picker.Item label="Female" value="female" />
+              <Picker.Item label="Male" value="male" />
+              <Picker.Item label="Others" value="others" />
+            </Picker>
           </View>
         </View>
       </View>
@@ -188,55 +192,55 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   caption: {
     fontSize: 14,
     lineHeight: 14,
-    fontWeight: '500',
+    fontWeight: "500",
   },
   row: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginBottom: 13,
   },
   infoBoxWrapper: {
-    borderBottomColor: '#dddddd',
+    borderBottomColor: "#dddddd",
     borderBottomWidth: 1,
-    borderTopColor: '#dddddd',
+    borderTopColor: "#dddddd",
     borderTopWidth: 1,
-    flexDirection: 'row',
+    flexDirection: "row",
     height: 100,
   },
   infoBox: {
-    width: '50%',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "50%",
+    alignItems: "center",
+    justifyContent: "center",
   },
   menuWrapper: {
     marginTop: 10,
   },
   menuItem: {
-    flexDirection: 'row',
+    flexDirection: "row",
     paddingVertical: 15,
     paddingHorizontal: 30,
   },
   menuItemText: {
-    color: '#777777',
+    color: "#777777",
     marginLeft: 20,
-    fontWeight: '600',
+    fontWeight: "600",
     fontSize: 16,
     lineHeight: 26,
   },
   header: {
     marginTop: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginHorizontal: 20,
-    justifyContent: 'space-between',
+    justifyContent: "space-between",
   },
   textBox: {
     height: 40,
-    alignItems: 'center',
+    alignItems: "center",
     paddingLeft: 20,
     flex: 1,
     backgroundColor: COLORS.secondary,
@@ -244,11 +248,11 @@ const styles = StyleSheet.create({
     borderBottomLeftRadius: 20,
     borderTopRightRadius: 20,
     borderBottomRightRadius: 20,
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   editTextBox: {
     height: 40,
-    alignItems: 'center',
+    alignItems: "center",
     paddingLeft: 10,
     flex: 1,
     backgroundColor: COLORS.secondary,
@@ -256,21 +260,21 @@ const styles = StyleSheet.create({
     borderBottomLeftRadius: 20,
     borderTopRightRadius: 20,
     borderBottomRightRadius: 20,
-    flexDirection: 'row',
+    flexDirection: "row",
     fontSize: 15,
   },
   button: {
     height: 52,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 130,
     backgroundColor: COLORS.primary,
     marginHorizontal: 20,
     borderRadius: 10,
   },
   buttonText: {
-    color: 'white',
-    fontWeight: '700',
+    color: "white",
+    fontWeight: "700",
     fontSize: 16,
   },
 });
